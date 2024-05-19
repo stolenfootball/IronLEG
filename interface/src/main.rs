66391499a0f9ce0e9c5a55eb84d93de5c4e12f3a
Jeremy Dunn <jeremy.dunn315@gmail.com>
2024-05-19 19:09:12 -0400

    Add latency to main memory
    Main memory can now differentiate between stages and can now be set
    with a latency for simulation purposes.

--- interface/src/main.rs
+++ interface/src/main.rs
@@ -1,11 +1,16 @@
 use memory::memory;
 
 fn main() {
-    let mut mem = memory::Memory::new(65536, 16, 4);
-    let result = mem.read(0).unwrap();
-    println!("Read result: {result}");
+    let mut mem = memory::Memory::new(65536, 16, 4, 5);
+    // let result = mem.read(0).unwrap();
+    // println!("Read result: {result}");
 
-    mem.write(0, 50);
-    let result = mem.read(0).unwrap();
-    println!("Read result: {result}");
+    for _ in 0..5 {
+        mem.write(0, 50, memory::PipelineStage::Memory);
+    }
+    let mut result: Option<u32> = None;
+    for _ in 0..5 {
+        result = mem.read(0, memory::PipelineStage::Fetch);
+    }
+    println!("Read result: {}", result.unwrap());
 }
--- memory/src/lib.rs
+++ memory/src/lib.rs
@@ -1,19 +1,42 @@
 
 pub mod memory {
     use std::convert::TryFrom;
+
+    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
+    pub enum PipelineStage {
+        Fetch,
+        Decode,
+        Execute,
+        Memory,
+        Writeback,
+    }
+
+    #[derive(Clone, Copy, Debug)]
+    pub struct MemoryAccess {
+        cycles_to_completion: i32,
+        stage: Option<PipelineStage>,
+    }
+
     pub struct Memory {
         size: u32,
         block_size: u32,
         word_size: u32,
+        latency: i32,
+        access: MemoryAccess,
         contents: Vec<Vec<u32>>,
     }
 
     impl Memory {
-        pub fn new(size: u32, block_size: u32, word_size: u32) -> Self {
+        pub fn new(size: u32, block_size: u32, word_size: u32, latency: i32) -> Self {
             Self {
                 size: size,
                 block_size: block_size,
                 word_size: word_size,
+                latency: latency,
+                access: MemoryAccess {
+                    cycles_to_completion: latency,
+                    stage: None,
+                },
                 contents: vec![vec![0, block_size]; size.try_into().unwrap()],
             }
         }
@@ -27,14 +50,37 @@ pub mod memory {
             (usize::try_from((addr % self.size) / self.block_size).unwrap(), usize::try_from(addr % self.block_size).unwrap())
         }
 
-        pub fn read(&self, addr: u32) -> Option<u32> {
+        fn attempt_access(&mut self, attempt_stage: PipelineStage) -> bool {
+            match self.access.stage {
+                Some(current_stage) => {
+                    if current_stage != attempt_stage { 
+                        return false; 
+                    }
+                    self.access.cycles_to_completion -= 1;
+                    return self.access.cycles_to_completion <= 1;
+                },
+                None => self.access.stage = Some(attempt_stage)
+            }
+            false
+        }
+
+        fn reset_access_state(&mut self) {
+            self.access.cycles_to_completion = self.latency;
+            self.access.stage = None;
+        }
+
+        pub fn read(&mut self, addr: u32, stage: PipelineStage) -> Option<u32> {
+            if !self.attempt_access(stage) { return None; }
             let addr = self.addr_to_offset(addr);
+            self.reset_access_state();
             Some(self.contents[addr.0][addr.1])
         }
 
-        pub fn write(&mut self, addr: u32, value: u32) -> Option<u32> {
+        pub fn write(&mut self, addr: u32, value: u32, stage: PipelineStage) -> Option<u32> {
+            if !self.attempt_access(stage) { return None; }
             let addr = self.addr_to_offset(addr);
             self.contents[addr.0][addr.1] = value;
+            self.reset_access_state();
             Some(value)
         }
     }
