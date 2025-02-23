b7ad94f5b586a8940b684366f9a1c6416689d8c5
Jeremy Dunn <jeremy.dunn315@gmail.com>
2025-02-23 16:04:14 -0500

    Add basic functions to test viability
    The LEG is alive!!!!

--- interface/src/main.rs
+++ interface/src/main.rs
@@ -1,26 +1,10 @@
-// use std::fs;
+use std::fs;
 
 use simulator;
-// use simulator::assembler;
+use simulator::assembler;
 
 
-// fn main() {
-//     let asm = fs::read_to_string("./test/test.leg").unwrap();
-//     let program = assembler::assemble(&asm);
-
-//     let mut simulator = simulator::Simulator::new();
-//     simulator.flash(&program);
-
-//     for _ in 0..20 {
-//         simulator.processor.cycle();
-//         println!("{:?}", simulator.processor.peek_pipeline_instrs());
-//         println!("{:?}", simulator.processor.peek_pipeline_status());
-//         println!("{:?}\n", simulator.peek_regs());
-//     }
-// }
-
-
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use std::sync::Mutex;
 
 struct SimulatorState {
@@ -32,16 +16,42 @@ async fn index() -> impl Responder {
     "Hello world!"
 }
 
+
+#[get("/registers")]
+async fn get_regs(data: web::Data<SimulatorState>) -> impl Responder {
+    let simulator = data.sim.lock().unwrap();
+    let regs = simulator.processor.peek_registers();
+
+    format!("{:?}", regs)
+}
+
+#[get("/step")]
+async fn step(data: web::Data<SimulatorState>) -> HttpResponse {
+    let mut simulator = data.sim.lock().unwrap();
+    simulator.processor.cycle();
+    
+    HttpResponse::Ok().body("1")
+}
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let asm = fs::read_to_string("./test/test.leg").unwrap();
+    let program = assembler::assemble(&asm);
+
+    let mut simulator = simulator::Simulator::new();
+    simulator.flash(&program);
+
     let sim = web::Data::new(SimulatorState {
-        sim: Mutex::new(simulator::Simulator::new()),
+        sim: Mutex::new(simulator),
     });
 
     HttpServer::new(move || {
         App::new()
             .app_data(sim.clone())
             .service(index)
+            .service(get_regs)
+            .service(step)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
